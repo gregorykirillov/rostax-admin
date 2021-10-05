79f7c17933284f5f391aa58e7aa27091decad3af
unknown <grisha_kirillov@mail.ru>
2021-10-05 23:13:37 +0300

    Added authentication form

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,24 +1,54 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {AdminContext} from '@components';
 import {useHistory} from 'react-router-dom';
+import {Button, Input, Space} from 'antd';
+
 
 function index() {
     const {isAuthenticated, setAuthenticated} = useContext(AdminContext);
+    
+    const [isLogging, setLogging] = useState(false);
+    const [password, setPassword] = useState('');
+
     const history = useHistory();
+
+    const handleLogin = () => {
+        setLogging(true);
+        
+        setTimeout(() => {
+            setLogging(false);
+            password == '1'
+                ? (setAuthenticated(true))
+                : (alert('Wrong password'));
+        }, 700);
+    };
 
     useEffect(() => {
         isAuthenticated && history.push('/categories');
     }, [isAuthenticated]);
 
-    const handleClick = () => setAuthenticated(true);
 
     return (
-        <div>
-            <h1 style={{marginBottom: '10px'}}>Страница авторизации</h1>
-            <button 
-                style={{border: '1px solid', padding: '5px 10px', borderRadius: '7px', cursor: 'pointer'}} 
-                onClick={() => handleClick()}>Login</button>
-        </div>
+        <form
+            onSubmit={e => {
+                e.preventDefault();
+                handleLogin(e);
+            }}
+        >
+            <h1>Страница авторизации</h1>
+            <Space direction="vertical">
+                <Input 
+                    disabled={isLogging}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <Button 
+                    htmlType="submit"
+                    disabled={isLogging}
+                >
+                    Login
+                </Button>
+            </Space>
+        </form>
     );
 }
 
